5a6ebf4621472bea8336d91f113369d11e8648ca
daffa485 <sanamntzk96@gmail.com>
2019-08-16 08:48:40 +0700

    My First Commit!
    CV Website

---
+++ dist/precache-manifest.820ea95b2d3a3b2b3dad9c376406e8f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fc9570e6506acd919677",
+    "url": "/js/chunk-vendors.e3243972.js"
+  },
+  {
+    "revision": "b624a05b16c22ffd1812",
+    "url": "/js/chunk-3f13becf.ea586a39.js"
+  },
+  {
+    "revision": "31ae4b1177804c6d164a",
+    "url": "/js/app.473d69f0.js"
+  },
+  {
+    "revision": "25af1193825e6a60f4a310e7ff161f42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd893ca126b35d177e88c191920c7e34",
+    "url": "/img/poto.cd893ca1.jpg"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "fc9570e6506acd919677",
+    "url": "/css/chunk-vendors.4ce97d4e.css"
+  },
+  {
+    "revision": "b624a05b16c22ffd1812",
+    "url": "/css/chunk-3f13becf.1670cca4.css"
+  }
+];
